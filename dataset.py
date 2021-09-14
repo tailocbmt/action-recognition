0918df7d81be11e3daf7b6dcc721ce8639049b15
chisa <locntse150150@fpt.edu.vn>
2021-09-14 13:33:29 +0700

    first commit

--- dataset.py
+++ dataset.py
@@ -1,17 +1,123 @@
+import pandas as pd
 import os
 from pathlib import Path
 
 import cv2
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
+from torchvision.transforms import Compose
+from torchvision.transforms._transforms_video import NormalizeVideo, ToTensorVideo, RandomHorizontalFlipVideo
 
+class TripletVideoDataset(Dataset):
+    r"""A Dataset for a folder of videos. Expects the directory structure to be
+    directory->[train/val/test]->[class labels]->[videos]. Initializes with a list 
+    of all file names, along with an array of labels, with label being automatically
+    inferred from the respective folder names.
+
+        Args:
+            directory (str): The path to the directory containing the train/val/test datasets
+            mode (str, optional): Determines which folder of the directory the dataset will read from. Defaults to 'train'. 
+            clip_len (int, optional): Determines how many frames are there in each clip. Defaults to 8. 
+        """
+
+    def __init__(self, csvPath, mode='train', clip_len=8):
+        self.dataframe = pd.read_csv(csvPath)
+        self.tripletTrain = self.dataframe.iloc['status'==mode, 0:3]
+        self.clip_len = clip_len
+
+        if mode=='train':
+            self.transform = Compose([
+                            ToTensorVideo(),
+                            NormalizeVideo(mean=[0.43216, 0.394666, 0.37645],std=[0.22803, 0.22145, 0.216989]),
+                            RandomHorizontalFlipVideo()])
+        else:
+            self.transform = Compose([
+                            ToTensorVideo(),
+                            NormalizeVideo(mean=[0.43216, 0.394666, 0.37645],std=[0.22803, 0.22145, 0.216989])
+            ])
+
+        # the following three parameters are chosen as described in the paper section 4.1
+        self.resize_height = 128  
+        self.resize_width = 171
+        self.crop_size = 112
+
+    def __getitem__(self, index):
+        # loading and preprocessing. TODO move them to transform classes
+        tripletPath = self.loadvideo(self.tripletTrain[index])
+        triplets = []
+        
+        for path in tripletPath:
+            buffer = self.crop(path, self.clip_len, self.crop_size)
+            buffer = self.normalize(buffer)
+            triplets.append(buffer)
+
+        return buffer   
+        
+    def loadvideo(self, fname):
+        # initialize a VideoCapture object to read video data into a numpy array
+        capture = cv2.VideoCapture(fname)
+        frame_count = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        # create a buffer. Must have dtype float, so it gets converted to a FloatTensor by Pytorch later
+        buffer = np.empty((frame_count, self.resize_height, self.resize_width, 3), np.dtype('float32'))
+
+        count = 0
+        retaining = True
+
+        # read in each frame, one at a time into the numpy buffer array
+        while (count < frame_count and retaining):
+            retaining, frame = capture.read()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # will resize frames if not already final size
+            # NOTE: strongly recommended to resize them during the download process. This script
+            # will process videos of any size, but will take longer the larger the video file.
+            if (frame_height != self.resize_height) or (frame_width != self.resize_width):
+                frame = cv2.resize(frame, (self.resize_width, self.resize_height))
+            buffer[count] = frame
+            count += 1
+
+        # release the VideoCapture once it is no longer needed
+        capture.release()
+
+        # convert from [D, H, W, C] format to [C, D, H, W] (what PyTorch uses)
+        # D = Depth (in this case, time), H = Height, W = Width, C = Channels
+        buffer = buffer.transpose((3, 0, 1, 2))
+
+        return buffer 
+    
+    def crop(self, buffer, clip_len, crop_size):
+        # randomly select time index for temporal jittering
+        time_index = np.random.randint(buffer.shape[1] - clip_len)
+        # randomly select start indices in order to crop the video
+        height_index = np.random.randint(buffer.shape[2] - crop_size)
+        width_index = np.random.randint(buffer.shape[3] - crop_size)
+
+        # crop and jitter the video using indexing. The spatial crop is performed on 
+        # the entire array, so each frame is cropped in the same location. The temporal
+        # jitter takes place via the selection of consecutive frames
+        buffer = buffer[:, time_index:time_index + clip_len,
+                        height_index:height_index + crop_size,
+                        width_index:width_index + crop_size]
+
+        return buffer                
+
+    def normalize(self, buffer):
+        # Normalize the buffer
+        # NOTE: Default values of RGB images normalization are used, as precomputed 
+        # mean and std_dev values (akin to ImageNet) were unavailable for Kinetics. Feel 
+        # free to push to and edit this section to replace them if found. 
+        buffer = self.transform(buffer)
+        return buffer
+
+    def __len__(self):
+        return len(self.fnames)
 
 class VideoDataset(Dataset):
     r"""A Dataset for a folder of videos. Expects the directory structure to be
     directory->[train/val/test]->[class labels]->[videos]. Initializes with a list 
     of all file names, along with an array of labels, with label being automatically
     inferred from the respective folder names.
-
         Args:
             directory (str): The path to the directory containing the train/val/test datasets
             mode (str, optional): Determines which folder of the directory the dataset will read from. Defaults to 'train'. 
@@ -110,8 +216,7 @@ class VideoDataset(Dataset):
 
     def __len__(self):
         return len(self.fnames)
-
-
+        
 class VideoDataset1M(VideoDataset):
     r"""Dataset that implements VideoDataset, and produces exactly 1M augmented
     training samples every epoch.
